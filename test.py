188c0b1d75ecf6bd4ebb4331c7b5ebcf7cae5e63
Johnny <47119111+lostmypillow@users.noreply.github.com>
2025-01-11 19:22:47 +0800

    completd experimental functionality, await testing

--- test.py
+++ test.py
@@ -1,18 +1,25 @@
-from lib.extractor import Extractor
-# from lib.captcha_solver import CaptchaSolver
-# import os
-import asyncio
-async def test():
-    mock_person = {
-      "personal_id": "F132481419",
-      "student_id": "090705",
-      "name": "洪翊鈞",
-      "year": 2007,
-      "month": 9,
-      "day": 20,
-      "status": "pending"
-    }
-    extractor = Extractor(mock_person)
-    await extractor.start()
+# from lib.extractor import Extractor
 
-asyncio.run(test())
+
+load_dotenv()
+
+from captcha_solver import CaptchaSolver
+c = CaptchaSolver()
+print("im clean")
+# print(os.environ)
+# import os
+# import asyncio
+# async def test():
+#     mock_person = {
+#       "personal_id": "F132481419",
+#       "student_id": "090705",
+#       "name": "洪翊鈞",
+#       "year": 2007,
+#       "month": 9,
+#       "day": 20,
+#       "status": "pending"
+#     }
+#     extractor = Extractor(mock_person)
+#     await extractor.start()
+
+# asyncio.run(test())
